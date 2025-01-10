cbcef5146f6fab20ba081494eafd07db06151d46
Hieu D. Nguyen <130839278+akisurils@users.noreply.github.com>
2025-01-10 18:34:00 -0500

    Add tailwind css

--- crud-app-demo.client/src/App.jsx
+++ crud-app-demo.client/src/App.jsx
@@ -2,22 +2,15 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Hello World is this is a simple application</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <h1>To-do app</h1>
+      <div>
+        This is a card
+        <button>Check</button>
+        <button>Delete</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
